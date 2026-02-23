472052f98f19d61be59b2e5e417516f5f533d437
John Colton <jcolton96@gmail.com>
2026-02-23 14:40:38 +0000

    feat: add s3 bucket

---
+++ terraform/main.tf
@@ -0,0 +1,14 @@
+module "website_bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "5.10.0"
+
+  bucket = "johnc-vue-website"
+  acl    = "private"
+
+  control_object_ownership = true
+  object_ownership         = "ObjectWriter"
+
+  versioning = {
+    enabled = false
+  }
+}
